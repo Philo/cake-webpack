4f8bf17464064adba947744750c9ba0674c130cc
Gary Ewan Park <gep13@gep13.co.uk>
2017-03-31 20:34:37 +0000

    AppVeyor Publish: 167a4874f9bb6e150183b8830de9c3b60931ecf6 Merge branch 'release/0.2.0'
    * release/0.2.0:
      (GH-3) Added Wyam Documentation
      (GH-2) Corrected cache invalidation
      (GH-2) Updated AppVeyor config file
      (GH-2) Refactoring to get basic build to work
      (GH-2) Updated gitignore file
      (GH-2) Added main Cake.Recipe files
      (GH-2) Added GRM Config file
      (GH-2) Removed GRM Config File
      (GH-2) Renamed AppVeyor file
      Upgrade Cake to 0.17.0

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,168 @@
+
+var camelCaseTokenizer = function (obj) {
+    var previous = '';
+    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+        var current = cur.toLowerCase();
+        if(acc.length === 0) {
+            previous = current;
+            return acc.concat(current);
+        }
+        previous = previous.concat(current);
+        return acc.concat([current, previous]);
+    }, []);
+}
+lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+        this.tokenizer(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"WebpackRunnerAliases",
+        content:"WebpackRunnerAliases",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
+        title:"WebpackRunner",
+        content:"WebpackRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:2,
+        title:"WebpackRunnerSettings",
+        content:"WebpackRunnerSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"NodeToolRunner",
+        content:"NodeToolRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:4,
+        title:"WebpackRunnerFactory",
+        content:"WebpackRunnerFactory",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:5,
+        title:"WebpackGlobalRunner",
+        content:"WebpackGlobalRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:6,
+        title:"WebpackLocalRunner",
+        content:"WebpackLocalRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:7,
+        title:"WebpackLocalRunnerSettings",
+        content:"WebpackLocalRunnerSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:8,
+        title:"WebpackBuildMode",
+        content:"WebpackBuildMode",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackRunnerAliases',
+        title:"WebpackRunnerAliases",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackRunner_1',
+        title:"WebpackRunner<TSettings>",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackRunnerSettings',
+        title:"WebpackRunnerSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/NodeToolRunner_1',
+        title:"NodeToolRunner<TSettings>",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackRunnerFactory',
+        title:"WebpackRunnerFactory",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackGlobalRunner',
+        title:"WebpackGlobalRunner",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackLocalRunner',
+        title:"WebpackLocalRunner",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackLocalRunnerSettings',
+        title:"WebpackLocalRunnerSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Webpack/Cake.Webpack/api/Cake.Webpack/WebpackBuildMode',
+        title:"WebpackBuildMode",
+        description:""
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
