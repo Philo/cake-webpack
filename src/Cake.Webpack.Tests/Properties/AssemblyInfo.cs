8e3ee5ac7fbd5a074d8b6630df4c1e6ac096b944
Gary Ewan Park <gep13@gep13.co.uk>
2017-03-31 21:16:14 +0100

    (GH-2) Refactoring to get basic build to work

--- src/Cake.Webpack.Tests/Properties/AssemblyInfo.cs
+++ src/Cake.Webpack.Tests/Properties/AssemblyInfo.cs
@@ -7,9 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Cake.Webpack.Tests")]
 [assembly: AssemblyDescription("Aliases to assist with running Webpack from Cake build scripts")]
-[assembly: AssemblyCompany("Phil Oyston")]
-[assembly: AssemblyProduct("Cake.Webpack.Tests")]
-[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
